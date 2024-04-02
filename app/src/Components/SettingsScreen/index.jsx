3394ce612a75a4a9486ae15a3c0468f2d62d08e4
Marty Heavey <mheavey@google.com>
2024-04-01 21:18:19 -0700

    make game 90 seconds instead of 60

--- app/src/Components/SettingsScreen/index.jsx
+++ app/src/Components/SettingsScreen/index.jsx
@@ -11,18 +11,18 @@ const SettingsScreen = ({
         <Modal showModal>
             <div className="Settings">
                 <div>
-                    QuickQ is a one minute daily trivia quiz. Once you start the
-                    game, a timer starts counting down one minute. During the
-                    game, a question will begin to appear one character at a
-                    time. If you think you know the answer to the question, hit
-                    the "Buzz" button and type in your answer. Hitting the
-                    "Buzz" button will stop new characters of the question from
-                    appearing. Your one minute timer is stopped while you are
-                    typing your answer, but you only have 15 seconds to submit
-                    your answer. You can also skip any question by pressing the
-                    "Skip" button. See how many questions you can answer
-                    correctly in one minute! Start a streak to see how many days
-                    in a row you can answer at least one question correctly.
+                    QuickQ is a 90 second daily trivia quiz. Once you start the
+                    game, a timer starts counting down. During the game, a
+                    question will begin to appear one character at a time. If
+                    you think you know the answer to the question, hit the
+                    "Buzz" button and type in your answer. Hitting the "Buzz"
+                    button will stop new characters of the question from
+                    appearing. Your timer is stopped while you are typing your
+                    answer, but you only have 15 seconds to submit your answer.
+                    You can also skip any question by pressing the "Skip"
+                    button. See how many questions you can answer correctly in
+                    90 seconds! Start a streak to see how many days in a row you
+                    can answer at least one question correctly.
                 </div>
                 <br />
                 <div>
--- app/src/constants.js
+++ app/src/constants.js
@@ -2,27 +2,15 @@ export const screens = {
     start: "start",
     game: "game",
     finish: "finish",
-    settings: "settings"
-}
+    settings: "settings",
+};
 
 export const BUZZ_SECONDS = 15;
-export const GAME_SECONDS = 60;
-
-export const questionsAndAnswers = [
-    ["This Russian composer's ballet, inspired by the works of Shakespeare, features a feud between two warring families and is set in 16th-century Verona.", "Sergei Prokofiev"],
-    ["This American inventor, who founded the National Geographic Society and served as its president for 45 years, is known for his contributions to the development of the telephone.", "Alexander Graham Bell"],
-    ["In Norse mythology, this god of thunder and lightning is often depicted wielding a hammer called Mjolnir and is the namesake of a day of the week.", "Thor"],
-    ["This African country, known for its ancient pyramids and the river that flows through it, is home to the cities of Cairo, Alexandria, and Giza.", "Egypt"],
-    ["This English naturalist, known for his theory of evolution by natural selection, published his seminal work \"On the Origin of Species\" in 1859.", "Charles Darwin"],
-    ["In the Bible, this \"Doubting\" disciple of Jesus Christ is known for questioning the resurrection until he physically touched Jesus' wounds.", "Thomas"],
-    ["This Italian city is built on 118 small islands and is famous for its gondolas and annual Carnival. It is known as the \"City of Canals\".", "Venice"],
-    ["This American poet, known for his experimental style and unconventional use of language, wrote the epic poem \"Song of Myself\" and is considered one of the fathers of American free verse.", " Walt Whitman"],
-    ["In Greek mythology, this daughter of Zues is often depicted wearing armor and carrying a spear. She is the godess of wisdom and war is said to have been born fully grown from the head of her father.", "Athena"],
-]
+export const GAME_SECONDS = 90;
 
 export const GREEN_CHECK_EMOJI_HTML = <span>&#9989;</span>;
 export const NEXT_TRACK_EMOJI_HTML = <span>&#9197;&#65039;</span>;
 export const RED_X_EMOJI_HTML = <span>&#10060;</span>;
-export const CLOCK_EMOJI_HTML = <span>&#128337;</span>
+export const CLOCK_EMOJI_HTML = <span>&#128337;</span>;
 
 export default screens;
