9904e928fb655cd24a5857537409aeca12e0b19d
Marty Heavey <mheavey@google.com>
2024-06-11 20:52:02 -0700

    add basic flask server to serve questions based on date

--- app/src/App.js
+++ app/src/App.js
@@ -142,23 +142,51 @@ function App() {
         }
     };
 
+    // useEffect(() => {
+    //     const readQuestionsFromFile = () => {
+    //         fetch("questions_with_categories.json")
+    //             .then((response) => {
+    //                 return response.json(); // Parse directly as JSON
+    //             })
+    //             .then((data) => {
+    //                 const questionSetToUse = daysPastApril142024();
+    //                 console.log("question set being used: " + questionSetToUse);
+    //                 setQuestions(data[questionSetToUse]);
+    //             })
+    //             .catch((error) => {
+    //                 console.error("Error fetching or parsing JSON:", error);
+    //             });
+    //     };
+
+    //     readQuestionsFromFile();
+    //     if (hasFinishedTodaysGame) {
+    //         const results = JSON.parse(
+    //             localStorage.getItem(new Date().toDateString())
+    //         );
+    //         setPlayerResults(results);
+    //     }
+    // }, [hasFinishedTodaysGame]);
+
     useEffect(() => {
-        const readQuestionsFromFile = () => {
-            fetch("questions_with_categories.json")
-                .then((response) => {
-                    return response.json(); // Parse directly as JSON
-                })
-                .then((data) => {
-                    const questionSetToUse = daysPastApril142024();
-                    console.log("question set being used: " + questionSetToUse);
-                    setQuestions(data[questionSetToUse]);
+        const fetchQuestionsFromServer = () => {
+            const today = new Date().toDateString();
+            const isoDateString = new Date(today).toISOString().split("T")[0]; // Get only the date part (YYYY-MM-DD)
+            // TODO: update this to a prod URL
+            fetch(`http://127.0.0.1:5000/api/questions/${isoDateString}`)
+                .then(async (response) => {
+                    const responseJson = await response.json();
+                    return setQuestions(responseJson);
                 })
                 .catch((error) => {
                     console.error("Error fetching or parsing JSON:", error);
                 });
         };
 
-        readQuestionsFromFile();
+        fetchQuestionsFromServer();
+        // TODO: is this the right time to refetch the questions?
+    }, [hasFinishedTodaysGame]);
+
+    useEffect(() => {
         if (hasFinishedTodaysGame) {
             const results = JSON.parse(
                 localStorage.getItem(new Date().toDateString())
