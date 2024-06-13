0571224affbfa2ea8f6ecca45954f2d9faa0f015
Marty Heavey <mheavey@google.com>
2024-06-13 10:51:38 -0700

    minor server updates

--- backend/server.py
+++ backend/server.py
@@ -9,16 +9,15 @@ cors = CORS(app)
 
 def get_questions_for_date(date):
     with open('backend_questions.json', 'r') as f:
-        data = json.load(f)
+        questions_list = json.load(f)
         question_set_to_use = calculate_days_past_april_14_2024(date)
-        return data[question_set_to_use]
-    
-def calculate_days_past_april_14_2024(input_date):
-    """Calculates the number of days that have passed since April 14, 2024.
+        return questions_list[question_set_to_use % len(questions_list)]
 
+def calculate_days_past_april_14_2024(input_date):
+    """
+    Calculates the number of days that have passed since April 14, 2024.
     Args:
         input_date (date): date object
-
     Returns:
         int: The number of days past April 14, 2024, or None if the input date is invalid.
     """
@@ -35,7 +34,7 @@ def calculate_days_past_april_14_2024(input_date):
         return None  # Return None if the input date format is invalid
 
 @app.route('/api/questions/<date_string>')
-def get_data_by_date(date_string):
+def get_data_by_date(date_string, methods=['GET']):
     """
       Args:
         date_string (str): The input date in the format 'YYYY-MM-DD'.
