dea5f01f332dfee12d4df431cbb827f9d5c70e69
Marty Heavey <mheavey@google.com>
2024-07-16 16:22:39 -0700

    add trailing space to try to fix share bug

--- app/src/Screens/FinishScreen/index.jsx
+++ app/src/Screens/FinishScreen/index.jsx
@@ -108,7 +108,8 @@ const FinishScreen = ({
             sharableResultsArr.push(row);
             i += 1;
         }
-        sharableResultsArr.push("https://QuickQTrivia.com");
+        // trailing space may help with URL encoding bug on iOS Safari > Share > Copy
+        sharableResultsArr.push("https://QuickQTrivia.com ");
         return sharableResultsArr.join("\n");
     };
 
