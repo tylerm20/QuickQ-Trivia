00cf3f3b8b840bc906788c9c28ba6c2907a0d8a5
Marty Heavey <mheavey@google.com>
2024-08-16 22:08:29 -0700

    reorganize some question things

--- questions/scripts/sheet_to_json.py
+++ questions/scripts/sheet_to_json.py
@@ -1,9 +1,8 @@
 import gspread
 import json
-import random
 from datetime import date
 
-def sheet_to_json(json_filepath):
+def sheet_to_json():
     gc = gspread.service_account()
 
     sh = gc.open("Quiz Questions")
@@ -26,12 +25,10 @@ def sheet_to_json(json_filepath):
                     row["answers"] = row["answers"].split(',')  # Modify this if your delimiter is different
                 
                 category_to_questions_unused[category].append(row)
-        random.shuffle(category_to_questions_unused[category])
 
-        file_name = '/Users/mheavey/personal/minquiz/questions/categories/ordered_qs/{}_formatted.json'.format(category.replace(" ", "_").lower())
+        file_name = '/Users/mheavey/personal/minquiz/questions/categories/{}_formatted.json'.format(category.replace(" ", "_").lower())
 
         with open(file_name, 'w', encoding='utf-8') as json_file:
             json.dump(category_to_questions_unused[category], json_file, indent=4)
 
-json_filepath = '/Users/mheavey/personal/minquiz/questions/categories/ordered_qs/current_events_formatted.json'
-sheet_to_json(json_filepath)
+sheet_to_json()
