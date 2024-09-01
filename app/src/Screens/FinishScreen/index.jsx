232f791544dcad2624b37f9ffb879e8e0df6ba48
Marty Heavey <mheavey@google.com>
2024-08-31 20:40:05 -0700

    more share testing

--- app/src/Screens/FinishScreen/index.jsx
+++ app/src/Screens/FinishScreen/index.jsx
@@ -115,7 +115,7 @@ const FinishScreen = ({
             sharableResultsArr.push(row);
             i += 1;
         }
-        sharableResultsArr.push("\nwww.QuickQTrivia.com \n");
+        sharableResultsArr.push("QuickQTrivia.com");
         return sharableResultsArr.join("\n");
     };
 
