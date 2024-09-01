79e96589402e937f45a8afd6c8a42896ac73e090
Marty Heavey <mheavey@google.com>
2024-08-31 21:27:15 -0700

    testing

--- app/src/Screens/FinishScreen/index.jsx
+++ app/src/Screens/FinishScreen/index.jsx
@@ -80,6 +80,7 @@ const FinishScreen = ({
             navigator.share({
                 title: "Results",
                 text: sharableResults,
+                // url: "quickqtrivia.com",
             });
         } else {
             navigator.clipboard.writeText(sharableResults).then(() => {
@@ -129,7 +130,7 @@ const FinishScreen = ({
             sharableResultsArr.push(row);
             i += 1;
         }
-        sharableResultsArr.push("https://QuickQTrivia.com");
+        sharableResultsArr.push("click this: QuickQTrivia.com");
         return sharableResultsArr.join("\n");
     };
 
