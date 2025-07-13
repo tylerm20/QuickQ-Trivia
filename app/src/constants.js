5754e367c88e953d0d04015f80cd945e89805b11
Tyler Maxey <tyler.maxey@capitalpreferences.com>
2025-07-13 23:32:17 +0200

    Adding questions

--- app/src/constants.js
+++ app/src/constants.js
@@ -76,7 +76,7 @@ export const GameModes = {
 
 
 export const FIRST_GAME_DATE = new Date(2024, 3, 14); // April 14, 2024
-export const LAST_GAME_DATE = new Date(2026, 3, 15); // April 12, 2025
-//export const LAST_QUESTIONS_INDEX = 364;
+export const LAST_GAME_DATE = new Date(2025, 3, 16); // April 12, 2025
+export const LAST_QUESTIONS_INDEX = 367;
 
 export default screens;
